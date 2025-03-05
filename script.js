5b848068b399f84323b028af8fc321ca65a07ab1
madam31-rrc <madam31@academic.rrc.ca>
2025-03-04 23:26:27 -0600

    feat: Update the scrip.js file

--- script.js
+++ script.js
@@ -65,12 +65,4 @@ document.getElementById("surveyForm").addEventListener("submit", function(event)
         return true;
     }
 
-    isValid &= isNotEmpty("name");
-    isValid &= isValidEmail("email");
-    isValid &= hasCheckedOption("satisfaction");
-    isValid &= isSelected("service");
     
-    if (!isValid) {
-        event.preventDefault();
-    }
-});
